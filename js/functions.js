8880f7c589c979aa5d8262a6e94f3a27f670676d
wensir <wenpengkun@kyee.com.cn>
2018-08-15 00:14:45 +0800

    样式更好看

--- js/functions.js
+++ js/functions.js
@@ -98,26 +98,10 @@ function startHeartAnimation() {
 })(jQuery);
 
 function timeElapse(date){
-	// var current = Date();
-	// var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	// var days = Math.floor(seconds / (3600 * 24));
-	// seconds = seconds % (3600 * 24);
-	// var hours = Math.floor(seconds / 3600);
-	// if (hours < 10) {
-	// 	hours = "0" + hours;
-	// }
-	// seconds = seconds % 3600;
-	// var minutes = Math.floor(seconds / 60);
-	// if (minutes < 10) {
-	// 	minutes = "0" + minutes;
-	// }
-	// seconds = seconds % 60;
-	// if (seconds < 10) {
-	// 	seconds = "0" + seconds;
-	// }
+	var current = getNowFormatDate();
     seconds =+ seconds+1;
 	// var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
-	var result = " <span class=\"digit\">" + seconds + "</span> seconds"; 
+	var result = " <span class=\"digit\">" + current + "</span> "; 
 	$("#elapseClock").html(result);
 }
 
@@ -140,4 +124,22 @@ function adjustCodePosition() {
 
 function showLoveU() {
 	$('#loveu').fadeIn(3000);
+}
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + date.getHours() + seperator2 + date.getMinutes()
+            + seperator2 + date.getSeconds();
+    return currentdate;
 }
